ec89d5d6ae75108c23a94a989ba2fb114d3823c9
ValterMonteiro <valtinms@gmail.com>
2023-04-07 10:59:50 -0400

    create component LocatorCard

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
-import { Menu } from '@mui/material';
+import { Menu, TextField } from '@mui/material';
 import { BaseLayout } from '../../Layout/BaseLayout';
+import { LocatorCard } from '../../LocatorCard';
 
 export function Home() {
   return (
     <>
       <BaseLayout appBarTitle='Localizador de Perfis'>
+        <LocatorCard />
       </BaseLayout>
     </>
   )
